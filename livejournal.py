f88086d5e8276db5ab24687c6f0402f6f2f22f2d
Zeta Syanthis <zeta@zetasyanthis.org>
2016-06-26 16:21:10 -0700

    Force https for LJ.

--- livejournal.py
+++ livejournal.py
@@ -5,7 +5,9 @@ from account_info import (EMAIL_ADDRESS, LJ_USER_AGENT, LJ_USERNAME, LJ_PASSWORD
 
 ljclient = lj.LJServer(
     "Python-Blog3/1.0",
-    LJ_USER_AGENT)
+    LJ_USER_AGENT,
+    # Force HTTPS since LJ uses *md5sum* for authentication. (WTF???)
+    host='https://www.livejournal.com/')
 ljclient.login(
     LJ_USERNAME,
     LJ_PASSWORD)
