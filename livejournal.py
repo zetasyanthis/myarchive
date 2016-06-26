cb931cedd488f1d63b1945539c36dddb241ba0dc
Zeta Syanthis <zeta@zetasyanthis.org>
2016-06-26 16:31:18 -0700

    Wrap the function.

--- livejournal.py
+++ livejournal.py
@@ -3,15 +3,21 @@ from lj import lj
 
 from account_info import (EMAIL_ADDRESS, LJ_USER_AGENT, LJ_USERNAME, LJ_PASSWORD)
 
-ljclient = lj.LJServer(
-    "Python-Blog3/1.0",
-    LJ_USER_AGENT,
-    # Force HTTPS since LJ uses *md5sum* for authentication. (WTF???)
-    host='https://www.livejournal.com/')
-ljclient.login(
-    LJ_USERNAME,
-    LJ_PASSWORD)
-ljclient.postevent(
-    "API Test post",
-    "API Test Subject",
-    props={"taglist": "github,livejournal"})
+
+def post_journal(subject, post, tags, host='https://www.livejournal.com/'):
+    """
+    Posts a journal to the specified LJ server.
+
+    WARNING: MUST use HTTPS since this API uses *md5sum* for authentication! D:
+    """
+    ljclient = lj.LJServer(
+        "Python-Blog3/1.0",
+        LJ_USER_AGENT,
+        host=host)
+    ljclient.login(
+        LJ_USERNAME,
+        LJ_PASSWORD)
+    ljclient.postevent(
+        event=post,
+        subject=subject,
+        props={"taglist": ",".join(tags)})
