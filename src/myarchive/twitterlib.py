de1241d419cdb9957aa4d7db072d5284f0e45cc6
Zeta Syanthis <zeta@zetasyanthis.org>
2016-07-13 22:15:55 -0700

    Handle goofy all() return.

--- src/myarchive/twitterlib.py
+++ src/myarchive/twitterlib.py
@@ -214,9 +214,12 @@ def import_from_csv(db_session, csv_filepath, username):
         sleep_on_rate_limit=True)
 
     print "Importing into CSVTweets..."
-    csv_tweets_by_id = dict(
-        (csv_tweet[0], csv_tweet)
-        for csv_tweet in db_session.query(CSVTweet).all())
+    existing_csv_tweets = db_session.query(CSVTweet).all()
+    if existing_csv_tweets:
+        csv_tweets_by_id = dict(
+            (csv_tweet[0], csv_tweet) for csv_tweet in existing_csv_tweets)
+    else:
+        csv_tweets_by_id = dict()
     with open(csv_filepath) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
