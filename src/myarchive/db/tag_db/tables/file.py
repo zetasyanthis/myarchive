c25626c60db54a69bb353ee52152fa17e8c5b5e4
Zeta Syanthis <zeta@zetasyanthis.org>
2017-07-02 17:52:00 -0700

    Fix up file imports to adjust extensions on files below 16MB.

--- src/myarchive/db/tag_db/tables/file.py
+++ src/myarchive/db/tag_db/tables/file.py
@@ -19,6 +19,8 @@ from myarchive.db.tag_db.tables.base import Base
 
 LOGGER = logging.getLogger(__name__)
 
+MAX_BUFFER = 16 * 2 ** 20
+
 
 class TrackedFile(Base):
     """Class representing a file managed by the database."""
@@ -60,12 +62,9 @@ class TrackedFile(Base):
         existing = False
         if file_buffer is not None:
             md5sum = md5(file_buffer).hexdigest()
-            # Detect an extension incase the URL doesn't have one.
-            extension = os.path.splitext(original_filename)[1]
-            if extension == '':
-                imghdr_extension = imghdr.what("", file_buffer)
-                if imghdr_extension:
-                    extension = "." + imghdr_extension
+            # Fix up extensions the URL doesn't have one.
+            extension = get_file_extension(
+                file_buffer=file_buffer, original_filename=original_filename)
             filepath = os.path.join(media_path, str(md5sum)) + extension
             tracked_file = db_session.query(cls).filter_by(md5sum=md5sum).all()
             if tracked_file:
@@ -78,7 +77,20 @@ class TrackedFile(Base):
                     fptr.write(file_buffer)
         elif copy_from_filepath is not None:
             original_filename = os.path.basename(copy_from_filepath)
-            extension = os.path.splitext(original_filename)[1]
+
+            # Fix up extensions incase they're wrong.
+            if os.path.getsize(copy_from_filepath) < MAX_BUFFER:
+                with open(copy_from_filepath, "rb") as fptr:
+                    file_buffer = fptr.read()
+                    extension = get_file_extension(
+                        original_filename=original_filename,
+                        file_buffer=file_buffer)
+            else:
+                extension = get_file_extension(
+                    original_filename=original_filename)
+
+            # Reopen just a pointer for md5sum, since we don't want to load
+            # massive files into memory.
             with open(copy_from_filepath, 'rb') as fptr:
                 md5sum = cls.get_file_md5sum(fptr=fptr)
             filepath = os.path.join(media_path, md5sum + extension)
@@ -124,3 +136,24 @@ class TrackedFile(Base):
                 break
             md5.update(data)
         return md5.hexdigest()
+
+
+def get_file_extension(original_filename, file_buffer=None):
+    """
+    Returns a fixed extension for a file buffer. (Many services, especially
+    furaffinity and twitter, sometimes return files with the wrong extension or
+    without extensions at all!)
+    """
+    filename, extension = os.path.splitext(original_filename)
+    if file_buffer is not None:
+        try:
+            imghdr_extension = "." + imghdr.what("", file_buffer)
+            if imghdr_extension == ".jpeg":
+                imghdr_extension = ".jpg"
+        except:
+            imghdr_extension = None
+        if imghdr_extension and imghdr_extension != extension:
+            LOGGER.warning("Fixing extension on '%s'. ('%s' -> '%s')",
+                           original_filename, extension, imghdr_extension)
+            return imghdr_extension
+    return extension
