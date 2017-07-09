70aaee2e56eb100161d95e7cf4acbb0ef5b2f32a
Zeta Syanthis <zeta@zetasyanthis.org>
2017-07-08 22:40:07 -0700

    Add error catch for user data pulls. Add cache for user data pulls.

--- src/myarchive/db/tag_db/tables/datables.py
+++ src/myarchive/db/tag_db/tables/datables.py
@@ -4,10 +4,10 @@ Module containing class definitions for files to be tagged.
 
 import logging
 
+from deviantart.api import DeviantartError
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import backref, relationship
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm.exc import NoResultFound
 
 from myarchive.db.tag_db.tables.association_tables import at_deviation_tag
 from myarchive.db.tag_db.tables.base import Base
@@ -17,6 +17,7 @@ from myarchive.db.tag_db.tables.file import TrackedFile
 LOGGER = logging.getLogger(__name__)
 
 
+EXISTING_USERNAME_CACHE = list()
 HASHTAG_REGEX = r'#([\d\w]+)'
 
 
@@ -42,9 +43,9 @@ class DeviantArtUser(Base):
     def __init__(self, userid, name, profile, stats, details):
         self.userid = userid
         self.name = name
-        self.profile = str(profile)
-        self.stats = str(stats)
-        self.details = str(details)
+        self.profile = profile
+        self.stats = stats
+        self.details = details
 
     def __repr__(self):
         return "<DeviantArtUser(name='%s')>" % self.name
@@ -90,12 +91,22 @@ def get_da_user(db_session, da_api, username, media_storage_path):
     Returns the DB user object if it exists, otherwise it grabs the user data
     from the API and stuffs it in the DB.
     """
+    global EXISTING_USERNAME_CACHE
+    if len(EXISTING_USERNAME_CACHE) == 0:
+        EXISTING_USERNAME_CACHE = [
+            username[0] for username in
+            db_session.query(DeviantArtUser.name).all()]
     # Grab the User object from the API.
-    user = da_api.get_user(username=username)
-    try:
+    if username in EXISTING_USERNAME_CACHE:
         da_user = db_session.query(DeviantArtUser).\
-            filter_by(name=user.username).one()
-    except NoResultFound:
+            filter_by(name=username).one()
+    else:
+        EXISTING_USERNAME_CACHE.append(username)
+        try:
+            user = da_api.get_user(username=username)
+        except DeviantartError:
+            LOGGER.error("Unable to obtain user data for %s", username)
+            return None
         da_user = DeviantArtUser(
             userid=user.userid,
             name=user.username,
